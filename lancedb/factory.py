772fbbfa32d1d6336fe13ab0c10eee78ec7b9a67
M K <mkpro118@gmail.com>
2024-06-24 17:54:57 -0500

    Made Configs hashable to use as dict keys

--- lancedb/factory.py
+++ lancedb/factory.py
@@ -1,17 +1,42 @@
+import dataclasses
+
 from lancedb.pydantic import LanceModel, Vector
 
-from snippets.lancedb.config import DBConfig, ST_Config
+from snippets.lancedb.config import DBConfig
 
 
-class SnippetSchemaFactory:
+@dataclasses.dataclass
+class SnippetSchemaFactory:  # type: ignore[misc]
+    registry: dict[DBConfig, type[LanceModel]] = dataclasses.field(default_factory=dict)
+
+    def register(self, config: DBConfig) -> None:
+        if not isinstance(config, DBConfig):
+            raise TypeError(
+                'Registration must be done with a DBConfig '
+                '(or a subclass) instance'
+            )
+
+        schema = SnippetSchemaFactory.make_schema(config)
+
+        self.registry[config] = schema
+
+    def get_schema(self, config: DBConfig) -> LanceModel:
+        if not isinstance(config, DBConfig):
+            raise TypeError(
+                f'Cannot get schema for type {type(config)} ',
+                'Registered schemas are DBConfig instances'
+            )
+
+        if config not in self.registry:
+            raise ValueError(f'No registered schemas for {config}')
+
+        return self.registry[config]
+
     @staticmethod
-    def get_schema(config: DBConfig) -> LanceModel:  # type: ignore[misc]
-        if isinstance(config, ST_Config):
-            return SnippetSchemaFactory._sentence_transformer(config)
-
-    @staticmethod
-    def _sentence_transformer(config: ST_Config) -> LanceModel:  # type: ignore[misc]
-        class SnippetSchema(LanceModel):  # type: ignore[misc]
+    def make_schema(config: DBConfig) -> LanceModel:  # type: ignore[misc]
+        class _Schema(LanceModel):  # type: ignore[misc]
             text: str = config.model.SourceField()
             vector: Vector(384) = config.model.VectorField()  # type: ignore[valid-type]
             language: str
+
+        return _Schema
