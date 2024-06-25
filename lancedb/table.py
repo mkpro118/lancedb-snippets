611865fa408c7c91810d62f92e09c38286a1dbc6
M K <mkpro118@gmail.com>
2024-06-24 19:19:38 -0500

    Add documents to table

--- lancedb/table.py
+++ lancedb/table.py
@@ -1,17 +1,21 @@
 import dataclasses
 
+from collections.abc import Iterable, Iterator
+
 from lancedb.table import Table
 
-from snippets.lancedb.config import ST_Config
+from snippets import Snippet
+from snippets.lancedb.config import DBConfig
 from snippets.lancedb.db_connection import SnippetDBConnection
 from snippets.lancedb.factory import SnippetSchemaFactory
 
 
 @dataclasses.dataclass
 class SnippetTable:
-    config: ST_Config
+    config: DBConfig
     name: str
     db: SnippetDBConnection
+    factory: SnippetSchemaFactory
     exist_ok: bool = False
 
     def __post_init__(self):
@@ -20,18 +24,25 @@ class SnippetTable:
 
         if db is None:
             raise ValueError(
-                f'SnippetDBConnection object {self.db} does not '
-                'define a lancedb.db.DBConnection. Invalid SnippetDBConnection'
+                f"SnippetDBConnection object {self.db} does not "
+                "define a lancedb.db.DBConnection. Invalid SnippetDBConnection"
             )
 
         try:
             self.table = db.create_table(
                 name=self.name,
-                schema=SnippetSchemaFactory.get_schema(config=self.config),
-                exist_ok=self.exist_ok
+                schema=self.factory.get_schema(config=self.config),
+                exist_ok=self.exist_ok,
             )
         except OSError:
             self.table = db.open_table(self.name)
 
-    # def add_snippets(self, snippet: Snippet):
-    #     pass
+    def add_snippets(
+        self, snippets: Snippet | Iterable[Snippet] | Iterator[Snippet]
+    ) -> None:
+
+        if isinstance(snippets, Snippet):
+            snippets = [snippets]
+
+        dicts = map(Snippet.to_dict, snippets)
+        self.table.add(dicts)
