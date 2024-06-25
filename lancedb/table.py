d881bd34a0a3640afe6636e404176c507207877f
M K <mkpro118@gmail.com>
2024-06-24 19:30:34 -0500

    Added search

--- lancedb/table.py
+++ lancedb/table.py
@@ -1,8 +1,12 @@
 import dataclasses
 
+import pandas as pd
+
 from collections.abc import Iterable, Iterator
+from typing import cast, Literal, Optional
 
 from lancedb.table import Table
+from lancedb.rerankers import Reranker
 
 from snippets import Snippet
 from snippets.lancedb.config import DBConfig
@@ -11,12 +15,13 @@ from snippets.lancedb.factory import SnippetSchemaFactory
 
 
 @dataclasses.dataclass
-class SnippetTable:
+class SnippetTable:  # type: ignore[misc]
     config: DBConfig
     name: str
     db: SnippetDBConnection
     factory: SnippetSchemaFactory
     exist_ok: bool = False
+    rerankers: list[Reranker] = dataclasses.field(default_factory=list)
 
     def __post_init__(self):
         self.table: Table
@@ -46,3 +51,26 @@ class SnippetTable:
 
         dicts = map(Snippet.to_dict, snippets)
         self.table.add(dicts)
+
+    def use_reranker(self, reranker: Reranker | Iterable[Reranker]) -> None:
+        self.rerankers.append(reranker)
+
+    def search(
+        self,
+        query: str,
+        language: Optional[str] = None,
+        limit: int | Literal[5] = 5,
+        use_rerankers: bool = True,
+    ) -> pd.DataFrame:
+        srch = self.table.search(query, query_type="hybrid")
+
+        if language:
+            srch = srch.where(f'language = "{language}"', prefilter=True)
+
+        if use_rerankers:
+            for reranker in self.rerankers:
+                srch = srch.rerank(reranker=reranker)
+
+        srch = srch.limit(limit=limit)
+
+        return cast(pd.DataFrame, srch.to_pandas())
