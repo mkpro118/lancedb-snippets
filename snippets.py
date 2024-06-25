2080afd6dfa2e8eceb92f50ef761ff0c8b0fcb57
M K <mkpro118@gmail.com>
2024-06-24 19:19:01 -0500

    Added method to convert to dict

--- snippets.py
+++ snippets.py
@@ -21,21 +21,21 @@ class Snippet:
         self.filename = filename.name
 
     @classmethod
-    def from_file(cls, filepath: str | pathlib.Path) -> 'Snippet':
+    def from_file(cls, filepath: str | pathlib.Path) -> "Snippet":
         if not isinstance(filepath, pathlib.Path):
             filepath = pathlib.Path(filepath).resolve()
         filepath.resolve()
 
         if not filepath.is_file():
             raise ValueError(
-                f'{filepath} does not exist or is not a regular file'
+                f"{filepath} does not exist or is not a regular file"
             )
 
         try:
             language = Language.from_file_extension(filepath)
         except ValueError:
             raise ValueError(
-                'Your file seems to be a language that is not yet supported'
+                "Your file seems to be a language that is not yet supported"
             )
 
         with open(str(filepath)) as f:
@@ -45,6 +45,6 @@ class Snippet:
 
     def to_dict(self) -> dict[str, str]:
         return {
-            'content': self.content,
-            'language': self.language.value,
+            "content": self.content,
+            "language": self.language.value,
         }
